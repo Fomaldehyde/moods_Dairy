368a8670b95fb72eb636217613cdcda87a78fdcf
fomaldehyde <1378638731@qq.com>
2025-04-06 16:12:32 +0800

    都改用bcryptjs

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 // 注册用户
